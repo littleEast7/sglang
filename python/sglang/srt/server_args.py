015da3da25db367f6cb93a2807e5f9f22bcbf7a6
littleEast7 <xiaodong163831@gmail.com>
2025-07-10 17:34:34 +0800

    add disk_cache

--- python/sglang/srt/server_args.py
+++ python/sglang/srt/server_args.py
@@ -87,6 +87,7 @@ class ServerArgs:
     hybrid_kvcache_ratio: Optional[float] = None
     swa_full_tokens_ratio: float = 0.8
     disable_hybrid_swa_memory: bool = False
+    use_multilevel_backend: bool = False
 
     # Runtime options
     device: Optional[str] = None
@@ -920,6 +921,12 @@ class ServerArgs:
             "--disable-hybrid-swa-memory",
             action="store_true",
             help="Disable the hybrid SWA memory.",
+        )
+        parser.add_argument(
+            "--use-multilevel-backend",
+            type=bool,
+            default=ServerArgs.use_multilevel_backend,
+            help="If using multi-level backend as kv cache.",
         )
 
         # Runtime options
